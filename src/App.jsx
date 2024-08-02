dbdbf36bc93c47bc37639159220f9f52003b05d6
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-08-02 20:47:02 +0530

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import {} from 'react-router-dom'
+import Navbar from './Components/Navbar'
 import './App.css'
-
-
 function App() {
-
-
   return (
     <>
-
+      <Navbar/>
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>
-        <div className='flex '>
-            
-        </div>
+    <div className="bg-zinc-700 flex justify-between p-5">
+    <h1 className="font-bold text-4xl">Logo</h1>
+    <div className="flex gap-3 items-center text-xl font-bold">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/Sign">Sign in</NavLink>
+      <NavLink to="/Login">Login</NavLink>
     </div>
+  </div>
   )
 }
 
