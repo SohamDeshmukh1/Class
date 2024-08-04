eb09be0315929b783c07a4dc74844cce313cb944
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-08-04 14:34:48 +0530

    Minor change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,12 @@
+import { NavLink } from 'react-router-dom'
 import React from 'react'
 export default function Navbar() {
   return (
     <>
-      <div>
-
+      <div className='bg-black text-white border border-white'>
+        <div className='flex'>
+          <NavLink to={} />
+        </div>
       </div>
     </>
   )
