75eab34272b1343d7299ad6cb1ee6896d0f262ed
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-08-04 13:22:22 +0530

    Understading Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import {Route,Routes} from 'react-router-dom'
 import Navbar from './Components/Navbar'
-import Home from './Pages/Home'
-import Login from './Pages/Login'
-import Signin from './Pages/Signin'
 import './App.css'
 function App() {
   return (
     <>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/Signin' element={<Signin/>} />
-      </Routes>
+      <Navbar/>  
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import Home from '../Pages/Home'
+import Login from '../Pages/Login'
+import Signin from '../Pages/Signin'
+import { NavLink, Route, Routes } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div className="bg-zinc-700 flex justify-between p-5">
-    <h1 className="font-bold text-4xl">Logo</h1>
-    <div className="flex gap-3 items-center text-xl font-bold">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/Signin">Sign in</NavLink>
-      <NavLink to="/Login">Login</NavLink>
+    <div>
+      <div className="bg-zinc-700 flex justify-between p-5">
+        <h1 className="font-bold text-4xl">Logo</h1>
+        <div className="flex gap-3 items-center text-xl font-bold">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/Signin">Sign in</NavLink>
+          <NavLink to="/Login">Login</NavLink>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/Signin' element={<Signin/>} />
+      </Routes>
     </div>
-  </div>
-  
+
   )
 }
 
